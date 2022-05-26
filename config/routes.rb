99e503d626094da6935e3215df98d38302391ada
Marina Muñoz <marinamuca01@gmail.com>
2022-05-26 23:02:01 +0200

    Inicio API

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: 'json' }  do
+    namespace :v1 do
+      resources :recipes
+    end
+  end
 end
