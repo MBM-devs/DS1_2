214a46ca74b22356c3ddc6ee0459127db907a303
Marina Muñoz <marinamuca01@gmail.com>
2022-06-05 17:50:15 +0200

    Merge develop

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :recipes_lists
       resources :shopping_lists
-<<<<<<< HEAD
-=======
       resources :recipe_ingredients
->>>>>>> develop
       resources :steps
       resources :recipes
       resources :ingredients
