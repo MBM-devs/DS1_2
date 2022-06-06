3efc680a2c1bbd2dc3914c41282c9abd2af9c205
Mario López González <mariolopezgonzalez2001@gmail.com>
2022-06-06 20:38:20 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_06_181215) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_181821) do
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "text"
@@ -59,6 +59,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_181215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["team"], name: "index_projects_on_team", unique: true
+  end
+
+  create_table "ratings", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ratings_on_recipe_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
   create_table "recipe_ingredients", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -122,6 +132,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_06_181215) do
   add_foreign_key "followings", "users", column: "followed", name: "followed_fk"
   add_foreign_key "followings", "users", column: "follower", name: "follower_fk"
   add_foreign_key "lists", "users"
+  add_foreign_key "ratings", "recipes"
+  add_foreign_key "ratings", "users"
   add_foreign_key "recipe_ingredients", "ingredients"
   add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "recipes_lists", "lists", on_update: :cascade, on_delete: :cascade
