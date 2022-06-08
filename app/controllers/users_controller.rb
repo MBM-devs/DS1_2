324c06d606739d1025c906d37cff4f8b1315384b
Marina Muñoz <marinamuca01@gmail.com>
2022-06-08 15:03:34 +0200

    Recetas guardadas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,7 @@ class UsersController < ApplicationController
       if @user.save
         session[:user_id] = @user.id # establece la sesión
         List.create(name: "Recetas de #{@user.username}", user_id:@user.id);
+        List.create(name: "Recetas Guardadas", user_id:@user.id);
         # format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.html { redirect_to root_path, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
